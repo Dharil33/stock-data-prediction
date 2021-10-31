2dd79765c8862b7dd02e9f8c7eba5f64738fb3f2
Dharil33 <59661721+Dharil33@users.noreply.github.com>
2021-10-31 21:56:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Register from './Register';
-import Login from './Login';
-import Home from "./Home"
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-// ReactDOM.render(
-//  <> "Hello"</>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <>
+   <Router>
+    <App />
+  </Router>
+  </>,
+  document.getElementById('root')
+);
 
-const routing = (  
-    <Router>  
-      <div>  
-            <center >
-        <div style={{width:"50%", display:"flex",marginTop:20, justifyContent:"space-around"}}>
-            <Link to="/" >Home</Link>
-            <Link to="/login" >Login</Link>
-            <Link to="/register" >Register</Link>
-        </div>
-            </center>
-        <Route exact path="/" component={Home} />  
-        <Route exact path="/register" component={Register} />  
-        <Route exact path="/login" component={Login} />  
-      </div>  
-    </Router>  
-  )
-ReactDOM.render(routing,document.getElementById('root'))
